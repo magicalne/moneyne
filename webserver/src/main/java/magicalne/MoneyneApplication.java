d6aa651b3800c54cb0ff94b44d33da0faa472c0e
zehui.lv <magicalne@gmail.com>
2017-05-01 13:58:44 +0800

    add webserver

---
+++ webserver/src/main/java/magicalne/MoneyneApplication.java
@@ -0,0 +1,12 @@
+package magicalne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyneApplication.class, args);
+	}
+}
